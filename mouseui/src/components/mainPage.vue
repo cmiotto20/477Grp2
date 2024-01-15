012e10ca1dafed02a4fae1b56e9bbdad9ba7f57d
mghera02 <mghera02@hotmail.com>
2024-01-14 19:31:04 -0500

    updated ip for API call

--- mouseui/src/components/mainPage.vue
+++ mouseui/src/components/mainPage.vue
@@ -11,7 +11,7 @@ export default {
   },
   methods: {
     turnOnLight() {
-      const url = 'http://3.94.110.51:8080/toggleLight'; 
+      const url = 'http://44.222.204.228:8080/toggleLight'; 
 
       fetch(url, {
         method: 'POST',
