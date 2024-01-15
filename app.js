d491a00b97ec9ac34a73b48b33b77496908eee46
root <root@ip-172-31-16-197.ec2.internal>
2024-01-15 06:35:29 +0000

    added toggle light api endpoint

--- app.js
+++ app.js
@@ -1,9 +1,64 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => {
-  res.status(200).send('OK');
+const fs = require('fs');
+
+// Use CORS middleware
+app.use(cors());
+
+app.get('/expectSuccess', (req, res) => {
+  res.status(200).send('This API call was a success (as expected)');
+});
+
+app.get('/expectFail', (req, res) => {
+  res.status(501).send('This API call was a fail (as expected)');
+});
+
+app.post('/toggleLight', (req, res) => {
+
+let ledStatus;
+
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+  
+    // Split the content into lines
+    const lines = data.split('\n');
+
+    let firstLine = lines[0];
+
+    // Split the string by colon
+    const parts = firstLine.split(':');
+
+    // Get the text after the colon (index 1)
+    const textAfterColon = parts[1].trim();
+
+    if(textAfterColon == "0") {
+      ledStatus = "1"
+    } else {
+      ledStatus = "0";
+    }
+  
+    // Change the first line (index 0)
+    lines[0] = `ledStatus:${ledStatus}`;
+  
+    // Join the lines back into a string
+    const updatedContent = lines.join('\n');
+  
+    // Write the updated content back to the file
+    fs.writeFile(filePath, updatedContent, 'utf8', (err) => {
+      if (err) {
+        console.error(err);
+        res.status(501).send(err);
+      }
+      });
+  i});
+
+  res.status(200).send('Light toggled');
 });
 
 app.listen(port, () => {
