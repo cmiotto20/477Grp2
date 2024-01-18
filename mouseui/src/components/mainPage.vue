cb19e715d8c78033a4c49859e70359a8f7b294b7
Braden Kirkendalll <bdkirkendall@yahoo.com>
2024-01-18 11:17:33 -0500

    added button components for directional movement

--- mouseui/src/components/mainPage.vue
+++ mouseui/src/components/mainPage.vue
@@ -1,6 +1,10 @@
 <template>
   <h1>MOUSE Controls Main Page</h1>
   <button @click="turnOnLight()">Toggle light</button>
+  <button @click="moveUp()">Up</button>
+  <button @click="moveRight()">Right</button>
+  <button @click="moveLeft()">Left</button>
+  <button @click="moveDown()">Down</button>
 </template>
 
 <script>
@@ -18,6 +22,88 @@ export default {
         headers: {
           'Content-Type': 'application/json', 
         },
+      })
+        .then(data => {
+          console.log('Response:', data);
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+    },
+    moveUp() {
+      const url = 'http://174.129.215.96:8080/moveUp'
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json', 
+        },
+        body: JSON.stringify({dir : "U"}),
+      })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+          }
+          return response.text();
+        })
+        .then(data => {
+          console.log('Response:', data);
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+    }, 
+    moveLeft() {
+      const url = 'http://174.129.215.96:8080/moveLeft'
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json', 
+        },
+        body: JSON.stringify({dir : "L"}),
+      })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+          }
+          return response.text();
+        })
+        .then(data => {
+          console.log('Response:', data);
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+    },
+    moveDown() {
+      const url = 'http://174.129.215.96:8080/moveDown'
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json', 
+        },
+        body: JSON.stringify({dir : "D"}),
+      })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+          }
+          return response.text();
+        })
+        .then(data => {
+          console.log('Response:', data);
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+    },
+    moveRight() {
+      const url = 'http://174.129.215.96:8080/moveLeft'
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json', 
+        },
+        body: JSON.stringify({dir : "R"}),
       })
         .then(response => {
           if (!response.ok) {
