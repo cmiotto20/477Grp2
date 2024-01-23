87b7c8e7871198883f7bf4ddbbd31a3246bd5c5a
Braden Kirkendalll <bdkirkendall@yahoo.com>
2024-01-22 22:19:53 -0500

    socket connections

--- app.js
+++ app.js
@@ -8,79 +8,129 @@ const wss = new WebSocketServer({ server });
 
 import {toggleLight, getLightStatus} from './apiFunctions.mjs'
 
+const clients = [];
+var micro_conn = null;
+
 wss.on('connection', (ws) => {
-    console.log('Client connected');
-    ws.on('message', (message) => {
-        console.log(`Received: ${message}`);
+  console.log('Client connected');
+  
+  ws.on('message', (message) => {
+    message = message.toString();
+    console.log(`Received: ${message}`); 
+      
+    switch(message) {
+      case "webpage":
+        clients.push(ws);
+        console.log(`New webpage connection (total: ${clients.length})`);
+        break;
 
-        if(message == "hello") {
-          ws.send(`Hi There`);
-        } else if(message == "toggle light") {
-          toggleLight((err, ledStatus) => {
-            if (err) {
-              console.error(`Error: ${err}`);
-            } else {
-              console.log(`Result: ${ledStatus}`);
-              ws.send(`ledStatus: ${ledStatus}`);
-            }
-          }); 
-        } else if(message == "get light status") {
-          getLightStatus((err, ledStatus) => {
-            if (err) {
-              console.error(`Error: ${err}`);
-            } else {
-              console.log(`${ledStatus}`);
-              ws.send(ledStatus);
-            }
-          }); 
+      case "micro":
+        micro_conn = ws;
+        console.log('Micro connected')
+        break;
+
+      case "hello": 
+        ws.send(`Hi There`);
+        break;
+
+      case "toggle light":
+        toggleLight((err, ledStatus) => {
+          if (err) {
+            console.error(`Error: ${err}`);
+          } else {
+            console.log(`Result: ${ledStatus}`);
+            ws.send(`ledStatus: ${ledStatus}`);
+          }
+        }); 
+        break;
+
+      case "get light status":
+        getLightStatus((err, ledStatus) => {
+          if (err) {
+            console.error(`Error: ${err}`);
+          } else {
+            console.log(`${ledStatus}`);
+            ws.send(ledStatus);
+          }
+        }); 
+        break;
+
+      case "mv R":
+        console.log("Received move R command");
+        if(micro_conn !== null){
+          console.log("Sending move R to micro");
+          //can send data to microcontroller connection here 
+          //i.e. micro_conn.send('R');
+          return;
         }
-    });
+        console.log("No micro connection");
+        break;
+      
+      case "mv L":
+        console.log("Received move L command");
+        if(micro_conn !== null){
+          console.log("Sending move L to micro");
+          //can send data to microcontroller connection here 
+          //i.e. micro_conn.send('L');
+          return;
+        }
+        console.log("No micro connection");
+        break;
 
-    ws.on('close', () => {
-        console.log('Client disconnected');
-    });
+      case "mv U":
+        console.log("Received move U command");
+        if(micro_conn !== null){
+          console.log("Sending move U to micro");
+          //can send data to microcontroller connection here 
+          //i.e. micro_conn.send('U');
+          return;
+        }
+        console.log("No micro connection");
+        break;
+
+      case "mv D":
+        console.log("Received move D command");
+        if(micro_conn !== null){
+          console.log("Sending move D to micro");
+          //can send data to microcontroller connection here 
+          //i.e. micro_conn.send('D');
+          return;
+        }
+        console.log("No micro connection");
+        break;
+
+      default: 
+        console.log('Error: invalid socket read');
+    }
+  });
+
+  ws.on('close', () => {
+      //check if removing webpage client
+      const client_idx = clients.indexOf(ws);
+      if(client_idx !== -1){
+        clients.splice(client_idx, 1);
+        console.log(`Webpage client disconnected (total: ${clients.length})`);
+        return;
+      }
+      //otherwise check if removing micro client
+      if(micro_conn == ws){
+        console.log('Microcontroller client disconnected');
+        micro_conn = null;
+        return;
+      }
+      console.log('Error: closing unidentified connection');
+  });
 });
 
-server.listen(3000, () => {
-    console.log('Server listening on port 3000');
-});
 
-/*const express = require('express');
-const cors = require('cors');
-const app = express();
-const port = 8080;
-
-const fs = require('fs');
-
-// Use CORS middleware
-app.use(cors());
-
-app.post('/moveUp', (req, res) => { 
+app.get('/move', (req, res) => { 
+    console.log("req: " + req)
     console.log('sending movement: Up'); 
     
     res.status(200).send(`U`);
   });
 
-app.post('/moveDown', (req, res) => {
-    console.log('sending movement: Down');
-    
-    res.status(200).send('D');
-  });
-
-app.post('/moveLeft', (req, res) => {
-    console.log('sending movement: Left');
-    
-    res.status(200).send('D');
-  });
-
-
-app.post('/moveRight', (req, res) => {
-    console.log('sending movement: Right');
-    
-    res.status(200).send('D');
-  });
-
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+server.listen(3000, () => {
+    console.log('Server listening on port 3000');
 });
-*/
+
--- mouseui/src/components/mainPage.vue
+++ mouseui/src/components/mainPage.vue
@@ -1,16 +1,14 @@
 <template>
   <h1>MOUSE Controls Main Page</h1>
   <button @click="toggleLight()">Toggle light</button>
-  <button @click="moveUp()">Up</button>
-  <button @click="moveRight()">Right</button>
-  <button @click="moveLeft()">Left</button>
-  <button @click="moveDown()">Down</button>
+  <button @click="moveAction('U')">Up</button>
+  <button @click="moveAction('R')">Right</button>
+  <button @click="moveAction('L')">Left</button>
+  <button @click="moveAction('D')">Down</button>
   <button @click="sendMessage()">Send Message</button>
 </template>
 
 <script>
-//const home = 'http://174.129.215.96:8080/'
-const home = 'http://localhost:8080/';
 export default {
   name: 'mainPage',
   data() {
@@ -23,103 +21,24 @@ export default {
     msg: String
   },
   methods: {
-    moveUp() {
-      const url = home + 'moveUp'
-      fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json', 
-        },
-        body: JSON.stringify({dir : "U"}),
-      })
-        .then(response => {
-          if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-          }
-          return response.text();
-        })
-        .then(data => {
-          console.log('Response:', data);
-        })
-        .catch(error => {
-          console.error('Error:', error);
-        });
-    }, 
-    moveLeft() {
-      const url = home + 'moveLeft'
-      fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json', 
-        },
-        body: JSON.stringify({dir : "L"}),
-      })
-        .then(response => {
-          if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-          }
-          return response.text();
-        })
-        .then(data => {
-          console.log('Response:', data);
-        })
-        .catch(error => {
-          console.error('Error:', error);
-        });
-    },
-    moveDown() {
-      const url = home + 'D'
-      fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json', 
-        },
-        body: JSON.stringify({dir : "D"}),
-      })
-        .then(response => {
-          if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-          }
-          return response.text();
-        })
-        .then(data => {
-          console.log('Response:', data);
-        })
-        .catch(error => {
-          console.error('Error:', error);
-        });
-    },
-    moveRight() {
-      const url = home + 'R'
-      fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json', 
-        },
-        body: JSON.stringify({dir : "R"}),
-      })
-        .then(response => {
-          if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-          }
-          return response.text();
-        })
-        .then(data => {
-          console.log('Response:', data);
-        })
-        .catch(error => {
-          console.error('Error:', error);
-        });
+    moveAction(dir) {
+      this.socket.send("mv " + dir)
     },
     sendMessage() {
-      this.socket.send('hello');
+      this.socket.send("hello");
     },
     toggleLight() {
-      this.socket.send('toggle light');
+      this.socket.send("toggle light");
     }
   },
   mounted() {
-    this.socket = new WebSocket('ws://174.129.215.96:3000');
+    //this.socket = new WebSocket('ws://174.129.215.96:3000');
+    this.socket = new WebSocket('ws://localhost:3000');
+    //wait for socket connection to be established
+    this.socket.onopen = () => {
+      this.socket.send("webpage");
+      console.log("Connection msg sent");
+    };
     this.socket.onmessage = (event) => {
       this.messages.push(event.data);
       console.log(event.data)
