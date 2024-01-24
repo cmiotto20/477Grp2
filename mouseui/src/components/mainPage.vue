0150a09afe681c1cb26a7a35697ed78b5693c013
mghera02 <mghera02@hotmail.com>
2024-01-24 15:31:01 -0500

    changed web ip

--- mouseui/src/components/mainPage.vue
+++ mouseui/src/components/mainPage.vue
@@ -41,8 +41,8 @@ export default {
     },
   },
   mounted() {
-    //this.socket = new WebSocket('ws://174.129.215.96:3000');
-    this.socket = new WebSocket('ws://localhost:3000');
+    this.socket = new WebSocket('ws://174.129.215.96:3000');
+    //this.socket = new WebSocket('ws://localhost:3000');
     //wait for socket connection to be established
     this.socket.onopen = () => {
       this.socket.send("[webpage]");
